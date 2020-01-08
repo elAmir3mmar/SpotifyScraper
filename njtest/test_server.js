36505a11a77ffe31dfcf041506a344416d8b5bdd
Ammar ElAmir <elamir.ammar@gmail.com>
2020-01-08 15:47:16 -0500

    init dir

---
+++ njtest/test_server.js
@@ -0,0 +1,12 @@
+//test_server.js
+
+/* Load the HTTP library */
+  var http = require("http");
+
+  /* Create an HTTP server to handle responses */
+
+  http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }).listen(8888);
