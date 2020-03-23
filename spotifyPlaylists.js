b83a62d0e257db1e0ea88106421cc6f90d6e0054
Ammar ElAmir <elamir.ammar@gmail.com>
2020-03-23 17:52:17 -0400

    Need to resolve asynchronous issue.

--- spotifyPlaylists.js
+++ spotifyPlaylists.js
@@ -1,12 +1,13 @@
 console.log("setting up vars");
 var fs = require('fs');
-
 var request = require("request");
+// var auth = require("./authorization_code/app.js");
+
 var userID = "ammarelamir";
-var token = "Bearer BQDm-sm5G6mEregvIYJ_004GNv5Y2w9XB0GS7MdqpYZUju0xyF-MBezEEEvEEBscCPAS3r_1JVuOAK_9wAFUnadmHoO2eJAiYmBwJOLJnJ0sOlqTUBw4VWCkg1Q9-COr2hWbz4wj2zCDkWRuGYmtluUZs3dx26CdZUuzV80_-mCIuhbi4cj6sg";
+var token = "Bearer  BQCJaZKLx_FCpzBuZmLXsZF0DwbOaPuLOTDgF9OPl7v7BJU_3l-CNfDYDJ3s-pUcxAsm4lzNbcelM7ie2KSEgsPJjDa7xU1q8IR73Oi534o6SmgxCgEvoNLTRJeVpRyQPr95mOm6b02AvXNRYvLxwmwCguqFzWWbU92JjuLmNJC7qdra";
 var offset = 0;
 var limit = 50;
-var playlistsURL = "https://api.spotify.com/v1/users/"+userID+"/playlists?offset="+offset+"&limit="+limit+"'";
+var playlistsURL = "https://api.spotify.com/v1/users/"+userID+"/playlists?limit=50&offset=0";
 var nextURL = playlistsURL;
 var logFile = "playlistsURL_res.txt"; 
 var sampleOutFile = "sampleOutFile.txt";
@@ -17,14 +18,74 @@ console.log("Next link (same): " + nextURL);
 
 while (nextURL != null){
 
-	console.log("Next URL :" + nextURL);
-	console.log("Count :   " + count);
+	console.log("Next URL: " + nextURL);
+	console.log("Count:    " + count);
 
 	count ++;
-	requestPlaylists(nextURL);
+	var playlistsJSON = requestPlaylists(nextURL);
+	//SO the problem here is that JS is asynchronous. 
+	// the code below runs even before the function call above returns...
+	console.log(playlistsJSON);
+// ///////////
+
+// 	console.log("Rquesting playlistsURL: " + playlistsURL);
+// 	request({
+//     url: playlistsURL,
+//     headers: {
+//         "Authorization": token
+//     }
+// 	}, function(err, res) {
+//         if (err) throw err;
+//         if (res) {
+// 		    var logFile = "wtf.txt"; 
+
+//             console.log(JSON.parse(res.body));
+//             fs.writeFile(logFile, JSON.stringify(JSON.parse(res.body), null, 4), function (err) {
+//     			if (err) throw err;
+//     		  	console.log('---Saved! ' + logFile);
+//     		});
+//     		var playlistsJSON = JSON.parse(res.body); 
+//     		console.log(playlistsJSON.total);
+//             // return playlistsJSON;
 
 
-}
+// }
+// }
+// )
+///////////////
+    // console.log(playlistsJSON.total);
+    // console.log(playlistsJSON.items);
+    //parsePlaylistsJSON(playlistsJSON);
+
+    // nextURL = playlistsJSON.next;
+    nextURL = null;
+
+} //END while nextURL
+
+function requestPlaylists(playlistsURL){
+	console.log("Rquesting playlistsURL: " + playlistsURL);
+	request({
+	    url: playlistsURL,
+	    headers: {
+	        "Authorization": token
+	    }
+		}, function(err, res) {
+			console.log("func");
+	        if (err) {
+	        	// nextURL = null;
+	        	console.log("Err here...");
+	        	throw err;
+	        }
+	        if (res) {
+	            var playlistsJSON = JSON.parse(res.body); 
+	            console.log(playlistsJSON);
+	    		console.log(playlistsJSON.total);
+			    console.log(playlistsJSON.items.length);
+	            return playlistsJSON;
+	        } //END if res
+		} //END function
+	) // END request call
+} //END function requestPlaylists
 
 function parsePlaylistsJSON(playlistsJSON){
 	var URL		= playlistsJSON.href;
@@ -33,52 +94,26 @@ function parsePlaylistsJSON(playlistsJSON){
 	var next	= playlistsJSON.next;
 	var offset	= playlistsJSON.offset;
 	var previous= playlistsJSON.previous;
-	var total	= playlistsJSON.total
+	var total	= playlistsJSON.total;
+
+	// fs.writeFile(logFile, JSON.stringify(playlistsJSON, null, 4), function (err) {
+	// 	if (err) throw err;
+	//   	console.log('---Saved! ' + logFile);
+	// });
+	
+	var firstPlaylist = playlistsJSON.items[0]; 
+    var playlistURL = firstPlaylist.href;
+    var playlistName = firstPlaylist.name;
+    var playlistOwner = firstPlaylist.owner.display_name;
+
+    console.log("Sample playlist URL: " + playlistURL);
+    console.log("Sample playlist name: " + playlistName);
+    console.log("Sample playlist owner: " + playlistOwner);
 } //END function parsePlaylistsJSON
 
-function requestPlaylists(playlistsURL){
-	console.log("Rquesting playlistsURL: " + playlistsURL)
-	request({
-	    url: playlistsURL,
-	    headers: {
-	        "Authorization": token
-	    }
-		}, function(err, res) {
-	        if (err) {
-	        	nextURL = null;
-	        	console.log("Err here...")
-	        	throw err;
-	        }
-	        if (res) {
-	            var playlistsJSON = JSON.parse(res.body); 
-	            console.log(playlistsJSON);
-	            
-	            parsePlaylistsJSON(playlistsJSON);
-
-	    		fs.writeFile(logFile, JSON.stringify(playlistsJSON, null, 4), function (err) {
-	    			if (err) throw err;
-	    		  	console.log('---Saved! ' + logFile);
-	    		});
-				
-				var firstPlaylist = playlistsJSON.items[0]; 
-	            var playlistURL = firstPlaylist.href;
-	            var playlistName = firstPlaylist.name;
-	            var playlistOwner = firstPlaylist.owner.display_name;
-
-	            console.log("Sample playlist URL: " + playlistURL);
-	            console.log("Sample playlist name: " + playlistName);
-	            console.log("Sample playlist owner: " + playlistOwner);
-
-	            console.log("Next URL: " + playlistsJSON.next);
-	            nextURL = playlistsJSON.next;
-	        }
-		}
-	) // END request call
-} //END function requestPlaylists
-
 function requestTracks(playlistItem){
-	console.log("Requesting tracks for playlist: " + playlistItem.name)
-	var playlistURL = playlistItem.href
+	console.log("Requesting tracks for playlist: " + playlistItem.name);
+	var playlistURL = playlistItem.href;
 	request({
 		url: playlistURL, 
 		headers: {
