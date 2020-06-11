81202126e30968dec2cc819b76d8d9f4289ddadc
Ammar ElAmir <elamir.ammar@gmail.com>
2020-06-11 00:34:15 -0400

    Using spotify-web-api-js to fetch playlists and track. Next: search for tracks.

--- README.txt
+++ README.txt
@@ -1,3 +1,24 @@
 The Spotify Playlist Scraper Project
 
 Collect data from my spotify playlists.
+
+NPM Libraries: 
+	spotify-web-api-js library to simplify requests
+	https://github.com/JMPerez/spotify-web-api-js
+
+	broserify to bundle libraries
+	
+	budo for live dev testing. 
+	
+main files: 
+	spotifyPlaylists2.js and index.html 
+
+
+
+Start: 
+	browserify spotifyPlaylists2.js > bundle.js
+or 
+	budo spotifyPlaylists2.js:bundle.js --live
+
+go to:
+http://192.168.2.43:9966/
--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+// import "spotifyPlaylists.js" as sp;
+// var scraper = require("spotifyPlaylists.js");
+import { spFunc } from './spotifyPlaylists.js';
 
-var rep = function () {
+var start = function () {
 
 	var logText = document.getElementById("logText");
 	if (logText.innerText == "") {
 		logText.innerText = "Text updated."
 	} else {
 		logText.innerText = ""
+	}
+	
+	if (require.main === module) {
+	    console.log('called directly');
+	} else {
+		console.log(spFunc());
 	}
 }
