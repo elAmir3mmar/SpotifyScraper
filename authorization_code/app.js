7b0b0350dc38bc6ecfbf019c2547595fc2156430
Ammar ElAmir <elamir.ammar@gmail.com>
2020-07-15 16:41:03 -0400

    Fixed redirect.

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -40,13 +40,14 @@ app.use(express.static(__dirname + '/public'))
    .use(cors())
    .use(cookieParser());
 
+
 app.get('/login', function(req, res) {
 
   var state = generateRandomString(16);
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email user-read-private playlist-read-private';
+  var scope = 'user-read-private user-read-email user-read-private playlist-read-private user-library-modify playlist-modify-public playlist-modify-private';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
