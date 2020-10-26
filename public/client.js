fd4802974cb72991690efe03486ddd5de50cf678
elAmir3mmar <elamir.ammar@gmail.com>
2020-10-25 22:45:06 -0400

    Updating lister layout

--- public/client.js
+++ public/client.js
@@ -12,8 +12,8 @@ var userId = "";
 // // https://github.com/JMPerez/promise-throttle
 const PromiseThrottle = require('promise-throttle');
 const promiseThrottle = new PromiseThrottle({
-    requestsPerSecond: 5,           // up to 5 request per second
-    promiseImplementation: Promise  // the Promise library you are using
+	requestsPerSecond: 5,           // up to 5 request per second
+	promiseImplementation: Promise  // the Promise library you are using
 });
 
 
@@ -30,15 +30,15 @@ document.getElementById("clear").addEventListener("click", clearCookies);
 
 function addEnter(element, button) {
 	var e = document.getElementById(element)
-	e.addEventListener("keyup", function(event) {
+	e.addEventListener("keyup", function (event) {
 		// Number 13 is the "Enter" key on the keyboard
 		if (event.keyCode === 13) {
-		  // Cancel the default action, if needed
-		  event.preventDefault();
-		  // Trigger the button element with a click
-		  document.getElementById(button).click();
+			// Cancel the default action, if needed
+			event.preventDefault();
+			// Trigger the button element with a click
+			document.getElementById(button).click();
 		}
-	  });
+	});
 }
 addEnter("filterInput", "filterPlaylists")
 addEnter("searchInput", "searchTracks")
@@ -50,39 +50,39 @@ document.getElementById("remX").addEventListener("click", xList);
 function getHashParams() {
 	// Copied from SP OAuth examples
 	console.log("GETTING HASH PARAMS")
-    var hashParams = {};
-    var e, 
-    	r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    e = r.exec(q)
-    while (e) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
-       e = r.exec(q);
-    }
-    return hashParams;
+	var hashParams = {};
+	var e,
+		r = /([^&;=]+)=?([^&;]*)/g,
+		q = window.location.hash.substring(1);
+	e = r.exec(q)
+	while (e) {
+		hashParams[e[1]] = decodeURIComponent(e[2]);
+		e = r.exec(q);
+	}
+	return hashParams;
 }
 
-function setCookie(cname, cvalue, exhrs=1) {
-  var d = new Date();
-  d.setTime(d.getTime() + (exhrs*60*60*1000));
-  var expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+function setCookie(cname, cvalue, exhrs = 1) {
+	var d = new Date();
+	d.setTime(d.getTime() + (exhrs * 60 * 60 * 1000));
+	var expires = "expires=" + d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function getCookie(cname) {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i <ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+	var name = cname + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
 }
 
 function clearCookies() {
@@ -104,29 +104,29 @@ if (params.access_token) {
 	setCookie("accessToken", params.access_token);
 	setCookie("refreshToken", params.refresh_token, 999999999);
 	console.log(document.cookie);
-	
+
 	sp.setAccessToken(params.access_token);
 
 } else if (getCookie("accessToken")) {
 	// if no params found check cookies 
 	console.log("found access token in cookie");
-		sp.setAccessToken(getCookie("accessToken"));
+	sp.setAccessToken(getCookie("accessToken"));
 
 } else {
-// if no access token found request a login
+	// if no access token found request a login
 	console.log("Please log in.");
 	//TODO use the refresh token here
 	window.location.href = "/login";
 }
 
-if (sp.getAccessToken()){
+if (sp.getAccessToken()) {
 	//if access token setup, proceed.
-	document.getElementById('loader').style.display = "none"; 
-	document.getElementById('buttons').style.display = "block"; 
+	document.getElementById('loader').style.display = "none";
+	// document.getElementById('buttons').style.display = "block"; 
 	document.getElementById("body").style.display = "block";
 
 	console.log("Token found.. Starting Automatically");
-	sp.getMe().then(r =>{
+	sp.getMe().then(r => {
 		console.log(r.id)
 		userId = r.id;
 	})
@@ -134,20 +134,20 @@ if (sp.getAccessToken()){
 }
 
 
-async function load(){
+async function load() {
 	// USER_PLAYLISTS = await getPlaylists(1);
 	getPlaylists(1)
-	.then(res => {
-		USER_PLAYLISTS = res
-		displayPlaylists(USER_PLAYLISTS)
-	})
-	.catch(err => {
-		console.log("Please log in.");
-		window.location.href = "http://localhost:8888";
-	})
+		.then(res => {
+			USER_PLAYLISTS = res
+			displayPlaylists(USER_PLAYLISTS)
+		})
+		.catch(err => {
+			console.log("Please log in.");
+			window.location.href = "http://localhost:8888";
+		})
 }
 
-async function getPlaylists(owned = 0){
+async function getPlaylists(owned = 0) {
 	if (owned) {
 		if (USER_PLAYLISTS.length == 0) {
 			USER_PLAYLISTS = await requestUserPlaylists(0, owned);
@@ -161,8 +161,8 @@ async function getPlaylists(owned = 0){
 	}
 }
 
-function toggleShow(){
-	if(this.checked){
+function toggleShow() {
+	if (this.checked) {
 		showMine()
 	}
 	else {
@@ -170,24 +170,24 @@ function toggleShow(){
 	}
 }
 
-function showAll(){
+function showAll() {
 	getPlaylists()
-	.then(r => {
-		displayPlaylists(ALL_PLAYLISTS)
-	})
+		.then(r => {
+			displayPlaylists(ALL_PLAYLISTS)
+		})
 }
 
-function showMine(){
+function showMine() {
 	getPlaylists(1)
-	.then(displayPlaylists(USER_PLAYLISTS))
+		.then(displayPlaylists(USER_PLAYLISTS))
 }
 
-async function requestUserPlaylists(count = 0, owned = 0){
+async function requestUserPlaylists(count = 0, owned = 0) {
 	//returns an array of JSON playlist objects
 	// console.log("requestUserPlaylists");
 
 	let id = ""
-	owned ? id = userId : "" 
+	owned ? id = userId : ""
 
 	// fileLog("Rquesting playlistsURL: " + playlistsURL);
 	let limit = 50;
@@ -197,22 +197,22 @@ async function requestUserPlaylists(count = 0, owned = 0){
 	let page = await promiseThrottle.add(
 		sp.getUserPlaylists.bind(
 			this,
-			options={"offset":count, "limit":limit}
+			options = { "offset": count, "limit": limit }
 		));
-	page.err ? console.log("FAIL " + page.err) : "" ;
+	page.err ? console.log("FAIL " + page.err) : "";
 	// console.log(page.items);
-	playlists = page.items; 
-	
-	let next 	= page.next;
-		count  += page.items.length;
-		total 	= page.total;
+	playlists = page.items;
 
-		log 	= `current: ${page.href}
+	let next = page.next;
+	count += page.items.length;
+	total = page.total;
+
+	log = `current: ${page.href}
 					\n Next: ${next}
 					\ncount: ${count}
 					\ntotal: ${total}\n\n
 					`;
-	
+
 	move("User Playlists", count, total)
 
 	if (next && count < total) {
@@ -227,7 +227,7 @@ async function requestUserPlaylists(count = 0, owned = 0){
 	return playlists;
 }
 
-async function requestPlaylistTracks(playlist, count=0) {
+async function requestPlaylistTracks(playlist, count = 0) {
 	//Returns an array of playlist track objects
 	// console.log("requesting tracks for: " + playlistID + " curr count: " + count);
 	playlistID = playlist.id
@@ -237,19 +237,19 @@ async function requestPlaylistTracks(playlist, count=0) {
 	let page = await promiseThrottle.add(
 		sp.getPlaylistTracks.bind(
 			this,
-			playlistID, 
-			options={"offset":count, "limit":limit}
+			playlistID,
+			options = { "offset": count, "limit": limit }
 		)
 	);
-	page.err ? console.log("FAIL " + page.err) : "" ;
-	playlistTracks = page.items; 
-	
-	let next 	= page.next;
-		count  += page.items.length;
-		total 	= page.total;
-	
+	page.err ? console.log("FAIL " + page.err) : "";
+	playlistTracks = page.items;
 
-		log 	= `current: ${page.href}
+	let next = page.next;
+	count += page.items.length;
+	total = page.total;
+
+
+	log = `current: ${page.href}
 					\n Next: ${next}
 					\ncount: ${count}
 					\ntotal: ${total}\n\n
@@ -258,12 +258,12 @@ async function requestPlaylistTracks(playlist, count=0) {
 	if (next && count < total) {
 		playlistTracks = playlistTracks.concat(
 			await requestPlaylistTracks(playlist, count)
-			);
+		);
 	}
-	playlistTracks = playlistTracks.map(t=>{
+	playlistTracks = playlistTracks.map(t => {
 		if (typeof t === "undefined" || t.track == null) return;
-		
-		if (t.added_at) t=t.track
+
+		if (t.added_at) t = t.track
 		t.playlist = playlist
 		return t;
 	})
@@ -272,15 +272,15 @@ async function requestPlaylistTracks(playlist, count=0) {
 	return playlistTracks;
 }
 
-function download(text, filename){
+function download(text, filename) {
 	var a = document.createElement('a');
-  	a.setAttribute('href', 'data:text/plain;charset=utf-8,'+encodeURIComponent(text));
-  	a.setAttribute('download', filename);
-  	a.click()
+	a.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+	a.setAttribute('download', filename);
+	a.click()
 
-  	// Call it:
-  	// var obj = {a: "Hello", b: "World"};
-  	// saveText( JSON.stringify(obj), "filename.json" );
+	// Call it:
+	// var obj = {a: "Hello", b: "World"};
+	// saveText( JSON.stringify(obj), "filename.json" );
 }
 
 function parseTracks() {
@@ -294,8 +294,8 @@ function parseTracks() {
 		let t = {}
 		t.name = c.innerText.split(",")[0]
 		t.id = c.id
-		t.playlistName = c.data-playlist-name
-		t.playlistId = c.data-playlist-id
+		t.playlistName = c.data - playlist - name
+		t.playlistId = c.data - playlist - id
 		tracks.push(t)
 	}
 
@@ -321,24 +321,24 @@ async function xList() {
 	document.getElementById("remX").style.display = "block";
 }
 
-function removeXTracks(){
+function removeXTracks() {
 	//remove xTracks from all playlists
 	// then remove from xList
 	let tracks = []
-	if (CURR_TRACKS.length==0) console.log("No Tracks Found")
-	
+	if (CURR_TRACKS.length == 0) console.log("No Tracks Found")
+
 	let playlists = mapPlaylists(CURR_TRACKS);
-	
-	trackIds = CURR_TRACKS.map(t => {return t.id})
-	trackUris = CURR_TRACKS.map(t => {return t.uri})
+
+	trackIds = CURR_TRACKS.map(t => { return t.id })
+	trackUris = CURR_TRACKS.map(t => { return t.uri })
 
 	// sp.removeFromMySavedTracks(trackIds).then(res => console.log(res))
-	
-	for (let id in playlists){
+
+	for (let id in playlists) {
 		// if(playlists[id].playlist.name.toUpperCase() == "XTHIS"){
 		// 	continue;
 		// }
-		let uris = playlists[id].tracks.map(t => {return t.uri})
+		let uris = playlists[id].tracks.map(t => { return t.uri })
 		console.log("REMOVING")
 		console.log(id, uris)
 		// sp.removeTracksFromPlaylist(id, uris).then(res => console.log(res))
@@ -347,14 +347,14 @@ function removeXTracks(){
 	// xList()
 }
 
-function mapPlaylists(tracks){
+function mapPlaylists(tracks) {
 	let playlists = {}
-	tracks.forEach(t=>{
+	tracks.forEach(t => {
 		if (!(t.playlist.id in playlists)) {
 			playlists[t.playlist.id] = {
-				"playlist": t.playlist, 
+				"playlist": t.playlist,
 				"tracks": []
-		};
+			};
 		}
 		playlists[t.playlist.id].tracks.push(t)
 	})
@@ -375,43 +375,43 @@ async function search() {
 	incrementalSearch(query, USER_PLAYLISTS);
 }
 
-async function filterPlaylists(query, playlists){
-	if (!(typeof(query) === 'string')) {
-		query = document.getElementById("filterInput").value.trim().toUpperCase(); 
+async function filterPlaylists(query, playlists) {
+	if (!(typeof (query) === 'string')) {
+		query = document.getElementById("filterInput").value.trim().toUpperCase();
 	}
 	console.log("searching for playlist: " + query);
 
-	if(!playlists){
+	if (!playlists) {
 		playlists = await getPlaylists()
 	}
 
-    let results = [];
+	let results = [];
 
-    results = playlists.filter(p => {
-    	let name = p.name.toUpperCase();
-    	let id = p.id.toUpperCase();
-    	let nameMatch = name.indexOf(query) !== -1;
-    	let idMatch = (id.localeCompare(query)) == 0;
-    	return ( nameMatch || idMatch );
-    });
-    console.log(results);
+	results = playlists.filter(p => {
+		let name = p.name.toUpperCase();
+		let id = p.id.toUpperCase();
+		let nameMatch = name.indexOf(query) !== -1;
+		let idMatch = (id.localeCompare(query)) == 0;
+		return (nameMatch || idMatch);
+	});
+	console.log(results);
 
-    displayPlaylists(results);
-    return results;
+	displayPlaylists(results);
+	return results;
 }
 
 
 //TODO change this to an incremental search 
 //.. Parse through each page of the playlistTracks then request the next page
-function incrementalSearch(query, playlists){
+function incrementalSearch(query, playlists) {
 
 	var pCount = 0;
 
-	playlists.forEach( p => {
+	playlists.forEach(p => {
 		queryPlaylist(p, query)
-		.then( r => {
-			move("playlists", ++pCount, playlists.length)
-		})
+			.then(r => {
+				move("playlists", ++pCount, playlists.length)
+			})
 
 	})
 }
@@ -419,26 +419,26 @@ function incrementalSearch(query, playlists){
 async function queryPlaylist(p, query) {
 	let results = []
 	// let rCount = 0;
-	
+
 	let tracks = await requestPlaylistTracks(p);
 	tracks.forEach(t => {
 		if (typeof t === "undefined") return;
 		name = t.name.toUpperCase();
-		if(name.indexOf(query) !== -1){
+		if (name.indexOf(query) !== -1) {
 			console.log(`found ${name} in ${p.name}`)
 
 			let rCount = document.getElementById("tracksList")
 				.getElementsByClassName("trackItem")
 				.length;
 
-			updateDisplay("tracksListTitle", `${rCount+1} tracks found...`);
+			updateDisplay("tracksListTitle", `${rCount + 1} tracks found...`);
 			displayAppend([t])
 		}
 	})
 	// return results;
 }
 
-function sameTrack(t1, t2){
+function sameTrack(t1, t2) {
 	//Check the type of the track object 
 	//(PlaylistTrack vs Track)
 	if (t1.track == null || t2.track == null) { return false; }
@@ -447,14 +447,14 @@ function sameTrack(t1, t2){
 	return t1.id == t2.id;
 }
 
-function filterTracks(source, filter){
+function filterTracks(source, filter) {
 	let r = []
 	filter.forEach(fT => {
 		r = r.concat(
-			source.filter(function(sT) {
+			source.filter(function (sT) {
 				if (sameTrack(sT, fT)) {
 					fT.found = true
-					return true 
+					return true
 				} else {
 					return false
 				}
@@ -464,35 +464,35 @@ function filterTracks(source, filter){
 	return r;
 }
 
-function findTracks(tracks, playlists){
+function findTracks(tracks, playlists) {
 	CURR_TRACKS = []
 	updateDisplay("tracksList", "");
 
 	let pCount = 0;
 	playlists.forEach(p => {
 		requestPlaylistTracks(p)
-		.then(pT => {
-			let r = filterTracks(pT, tracks)
-			rCount += r.length
-			updateDisplay("playlistLog", `${p.name} ${p.id} ${r.length}`)
-			r.forEach(t => {
-				t.playlist = p
-				displayAppend([t])
-				CURR_TRACKS.push(t)
+			.then(pT => {
+				let r = filterTracks(pT, tracks)
+				rCount += r.length
+				updateDisplay("playlistLog", `${p.name} ${p.id} ${r.length}`)
+				r.forEach(t => {
+					t.playlist = p
+					displayAppend([t])
+					CURR_TRACKS.push(t)
+				})
+				move("playlists", ++pCount, playlists.length)
 			})
-			move("playlists", ++pCount, playlists.length)
-		})
 	})
 }
 
-function updateDisplay(displayID, content){
+function updateDisplay(displayID, content) {
 	document.getElementById(displayID).innerText = content;
 
 }
 
-function displayPlaylists(playlists){
+function displayPlaylists(playlists) {
 	//Takes an array of JSON playlist objects 
-	if(!playlists){
+	if (!playlists) {
 		console.log(playlists)
 		throw "Playlists Required"
 	}
@@ -502,17 +502,17 @@ function displayPlaylists(playlists){
 	let list = document.getElementById("playlistsList");
 	list.innerHTML = "";
 
-	playlists.forEach(function(p) {
+	playlists.forEach(function (p) {
 		let div = document.createElement("li")
-		
-		let button = document.createElement("button");
-		let ptext = `<strong>${p.name}</strong><br/>${p.owner.id}`;
-		button.className = "w3-button w3-round-large w3-block w3-left-align w3-grey"
-		button.innerHTML = ptext;
-		button.id = p.id;
-		button.onclick = viewPlaylist;
 
-		div.append(button);
+		// let button = document.createElement("div");
+		let ptext = `<strong class="playlistName">${p.name}</strong><a href="#" class="ownerName">${p.owner.id}</a>`;
+		div.className = "playlistItem"
+		div.innerHTML = ptext;
+		div.id = p.id;
+		div.onclick = viewPlaylist;
+
+		// div.append(button);
 		list.append(div);
 	})
 
@@ -526,11 +526,11 @@ async function viewPlaylist() {
 	console.log(this)
 	let thisId = this.id
 	let playlist = await getPlaylists()
-	playlist = playlist.filter(p => {return thisId == p.id})
+	playlist = playlist.filter(p => { return thisId == p.id })
 
 	let playlistTracks = await requestPlaylistTracks(playlist[0]);
 	console.log("found playlist Tracks to view. Now display...");
-	updateDisplay("tracksListTitle", playlistTracks.length + " in playlist");
+	updateDisplay("tracksListTitle", playlistTracks.length + " tracks in selected playlist");
 
 	displayAppend(playlistTracks);
 }
@@ -540,47 +540,47 @@ async function viewPlaylist() {
 	Takes a destination and a list of items (tracks with playlists) to display.
 */
 function displayAppend(tracks) {
-	let destination="tracksList";
+	let destination = "tracksList";
 	let list = document.getElementById(destination);
 
 
-	tracks.forEach( t => {
+	tracks.forEach(t => {
 		if (typeof t === 'undefined') return;
-		if(t.added_at) t = t.track
-		console.log(t)
+		if (t.added_at) t = t.track
+		// console.log(t)
 
 		div = document.createElement("li")
 		div.className = "trackItem";
 
-		text 	= `<strong>${t.name}</strong><br/>${t.artists[0].name}<br/>${t.playlist.name}\n`;
+		text = `<strong class="trackName">${t.name}</strong><p class="artistName">${t.artists[0].name}</p><a href="#" class="inPlaylist">${t.playlist.name}</a>`;
 		div.id = t.id;
 		div.data = t;
 		div.dataset.playlistId = t.playlist.id;
 		div.dataset.playlistName = t.playlist.name;
 		div.innerHTML = text;
-		div.onclick = function(){console.log(this.data)}
-		list.append(div);	
-		div.scrollIntoView({behavior: "smooth"});
+		div.onclick = function () { console.log(this.data) }
+		list.append(div);
+		div.scrollIntoView({ behavior: "smooth" });
 	})
 }
 
 function move(label, count, total) {
-  var elem = document.getElementById("progressBar");   
-  var width = 0;
-  var id = setInterval(frame, 100);
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-      elem.innerHTML = "Checked all " + count + " " + label;
-    } else {
-    	while(width < (count/total)*100){
-	      width++; 
-	      elem.style.width = width + '%'; 
-	      var num = width * 1 / 10;
-	      num = num.toFixed(0)
-	      document.getElementById("progressBar").innerHTML = `${count}/${total}`;
+	var elem = document.getElementById("progressBar");
+	var width = 0;
+	var id = setInterval(frame, 100);
+	function frame() {
+		if (width >= 100) {
+			clearInterval(id);
+			elem.innerHTML = "Checked all " + count + " " + label;
+		} else {
+			while (width < (count / total) * 100) {
+				width++;
+				elem.style.width = width + '%';
+				var num = width * 1 / 10;
+				num = num.toFixed(0)
+				document.getElementById("progressBar").innerHTML = `${count}/${total}`;
 
-    	}	
-    }
-  }
+			}
+		}
+	}
 }
